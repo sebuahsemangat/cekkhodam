b0166352f9075c02491bfdfa31501f4c9a7c56dc
sebuahsemangat <103299640+sebuahsemangat@users.noreply.github.com>
2024-10-14 10:54:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import './style.css';
 // Get your Gemini API key by:
 // - Selecting "Add Gemini API" in the "Project IDX" panel in the sidebar
 // - Or by visiting https://g.co/ai/idxGetGeminiKey
-let API_KEY = 'AIzaSyD5uC4gAVexWnWpEV5MHfZaDwrpp8ULuT0';
+let API_KEY = 'AIzaSyCIWVvMFmcXJe9WUIsMLYUR_Ge8XLjt_iI';
 
 let form = document.querySelector('form');
 let promptInput = document.querySelector('input[name="prompt"]');
